49d228658fa500cde6992139ce6d3b7dd69ece8b
Athul Thomas <thomasathul@gmail.com>
2021-04-20 03:35:59 +0530

    New changes

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 import cv2
 import numpy as np
 
-
 app=Flask(__name__)
 UPLOAD_FOLDER = "C:/Users/Dell/Documents/ricedisease/static"
 
@@ -14,25 +13,45 @@ UPLOAD_FOLDER = "C:/Users/Dell/Documents/ricedisease/static"
 
 def predict(image_path):
     img = cv2.imread(image_path)
-    img = cv2.resize(img, (224, 224) )
+    img = cv2.resize(img, (224, 224),  interpolation=cv2.INTER_AREA)
     img = img /255
     img=np.asarray([img])
     p = MODEL.predict(img)
     p2= MODEL2.predict(img)
     p3=MODEL3.predict(img)
     diseases = ["Healthy", "Bacterial Leaf Blight", "Brown Spot", "Leaf Smut"]
-    #diseases = ["Leaf Smut", "Brown Spot", "Leaf Blast","Healthy", "Bacterial Leaf Blight"]
-    accu1=np.max(p)
-    accu2=np.max(p2)
-    accu3=np.max(p3)
-    if (accu1>accu2) and (accu1>accu3) :
-        class_idx = np.argmax(p)
-    elif (accu2 > accu1) and (accu2 > accu3):
-        class_idx = np.argmax(p2)
-    elif (accu3>accu1) and (accu3>accu1):
-        class_idx=np.argmax(p3)    
+    accu1=round(np.max(p)*100,2)
+    accu2=round(np.max(p2)*100,2)
+    accu3=round(np.max(p3)*100,2)
+    totaccu=accu1+accu2+accu3
+    healthaccu=0
+    blightaccu=0
+    brownaccu=0
+    smutaccu=0
+    list=[p,p2,p3]
+    for i in list:
+        if diseases[np.argmax(i)] =="Healthy":
+            healthaccu += round(np.max(i)*100, 2)
+        elif diseases[np.argmax(i)] == "Bacterial Leaf Blight":
+            blightaccu += round(np.max(i)*100, 2)
+        elif diseases[np.argmax(i)] == "Brown Spot":
+            brownaccu += round(np.max(i)*100, 2)
+        elif diseases[np.argmax(i)] == "Leaf Smut":
+            smutaccu += round(np.max(i)*100, 2) 
+    healthaccu=round((healthaccu/totaccu)*100,2)
+    blightaccu = round((blightaccu/totaccu)*100,2)
+    brownaccu =round((brownaccu/totaccu)*100,2)
+    smutaccu = round((smutaccu/totaccu)*100,2)
+    if healthaccu>blightaccu and healthaccu>brownaccu and healthaccu>smutaccu:
+        class_idx=0  
+    elif blightaccu > healthaccu and blightaccu > brownaccu and blightaccu > smutaccu:
+        class_idx=1
+    elif brownaccu > healthaccu and brownaccu > blightaccu and brownaccu > smutaccu:
+        class_idx = 2
+    elif smutaccu > healthaccu and smutaccu > blightaccu and smutaccu > brownaccu:
+        class_idx = 3   
     a = int(class_idx)
-    return diseases[a],diseases[np.argmax(p)],diseases[np.argmax(p2)],diseases[np.argmax(p3)],accu1,accu2,accu3
+    return diseases[a], healthaccu, blightaccu, brownaccu, smutaccu
 
 
 
@@ -45,12 +64,16 @@ def upload_predict():
                 UPLOAD_FOLDER, image_file.filename
                 )
             image_file.save(image_location)
-            predict_value,iv,mv,nv,accu1,accu2,accu3=predict(image_location)
-            return render_template("index.html", prediction=predict_value, image_loc=image_file.filename,incepval=iv,mobilval=mv,nasval=nv,incecon=round(accu1*100,2),mobilcon=round(accu2*100,2),nascon=round(accu3*100,2))
-    return render_template("index.html", prediction=" ", image_loc=None,incepval=None,mobilval=None,nasval=None,incepcon=None,mobilcon=None,nascon=None)
+            predict_value, accu1, accu2, accu3,accu4 = predict(image_location)
+         
+            return render_template("index.html", prediction=predict_value, image_loc=image_file.filename, health=accu1, blight=accu2, spot=accu3, smut=accu4)
+    return render_template("index.html", prediction=" ", image_loc=None, health=None,blight=None,spot=None,smut=None)
+
+ 
 
 if __name__ == "__main__":
     MODEL = keras.models.load_model("C:/Users/Dell/Documents/ricedisease/savedmodel/ince_model.h5")
     MODEL2 = keras.models.load_model("C:/Users/Dell/Documents/ricedisease/savedmodel/mobile_model.h5")
     MODEL3 = keras.models.load_model("C:/Users/Dell/Documents/ricedisease/savedmodel/nasnet_model.h5")
     app.run(port=8080,debug=True)    
+
